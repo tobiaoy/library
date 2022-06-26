004e0d5887fefd89e36845c909f12d88943addab
Tobi Oyero <tobioyero@gmail.com>
2022-06-25 21:44:02 -0400

    Skeleton page created

---
+++ app.js
@@ -0,0 +1,16 @@
+//DOM stuff to get form values
+
+// click add book button
+// triggers form opening
+// user fills form with data
+// data is loaded from form to js
+// data is used to make a book object
+// book object is rendered into the shelf div
+// shelf div renders each object as a card
+// method should not re-render already rendered books
+// use id to know what books have already been rendered
+// book cards should have two buttons: delete and read
+// delete button should delete the card and the actual book from storage (array)
+// read button should toggle between read and unread status
+
+
