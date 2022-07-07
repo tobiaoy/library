675cfc09ae0ae225ef5c966cd5e3e84db9c47697
Tobi Oyero <tobioyero@gmail.com>
2022-07-07 13:08:56 -0400

    Added card making functionality

--- app.js
+++ app.js
@@ -13,4 +13,133 @@
 // delete button should delete the card and the actual book from storage (array)
 // read button should toggle between read and unread status
 
+let library = [];
+const shelf = document.querySelector('#shelf');
 
+let saveButton = document.querySelector('#save-book');
+
+function readBool(readStatus){  
+    if (readStatus.checked) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function Book(title, author, pageCount, readStatus){
+  this.title = title;
+  this.author = author;
+  this.pageCount = pageCount;
+  this.readStatus = readStatus;
+}
+
+function makeBook(){
+	let bookTitle = document.querySelector('#title').value;
+	let bookAuthor = document.querySelector('#author').value;
+	let bookPcount = parseInt(document.querySelector('#page-count').value);
+	let bookRead = readBool(document.querySelector('#read-status'))
+	
+  if ((bookTitle !== null && bookTitle !== '') && (bookAuthor !== null && bookAuthor !== '') && (bookPcount !== null && bookPcount !== '')){
+	return new Book(bookTitle, bookAuthor, bookPcount, bookRead)
+  } else return;
+}
+
+function clearForm(){
+document.querySelector('#title').value = '';
+document.querySelector('#author').value = '';
+document.querySelector('#page-count').value = '';
+document.querySelector('#read-status').checked = false;  
+}
+
+function clearEmpties(book){
+return book !== undefined;
+}
+
+ function addBookToLibrary(){
+	library.push(makeBook());
+  library = library.filter(clearEmpties)
+  clearForm();
+ 
+ }
+
+saveButton.addEventListener('click', addBookToLibrary);
+
+function renderBook(book){
+    let bookCard = document.createElement('div');
+    bookCard.classList.add('book-card');
+    
+    //card title
+    let cardTitle = document.createElement('h2');
+    cardTitle.textContent = `${book.title}`;
+    cardTitle.classList.add('card-title');
+    
+    //card author
+    let cardAuthor = document.createElement('p');
+    cardAuthor.textContent = `Author: ${book.author}`;
+    cardAuthor.classList.add('card-author')
+    
+    //card page count
+    let cardPages = document.createElement('p');
+    cardPages.textContent = `Page Count: ${book.pageCount}`;
+    cardPages.classList.add('card-pages')
+    
+    //readstatus toggle
+    let cardRead = document.createElement('button');
+    cardRead.setAttribute('id', 'read-toggle');
+    if (book.readStatus === true){
+    cardRead.classList.add('completed-book');
+      cardRead.textContent = 'Read';
+    } else {
+      cardRead.classList.add('incomplete-book');
+      cardRead.textContent = 'Unread';
+    }
+    
+    //delete button
+    const cardDelete = document.createElement('button');
+    cardDelete.setAttribute('id', 'delete-card');
+    cardDelete.textContent = `Delete`;
+    
+    bookCard.appendChild(cardTitle);
+    bookCard.appendChild(cardAuthor);
+    bookCard.appendChild(cardPages);
+    bookCard.appendChild(cardRead);
+    bookCard.appendChild(cardDelete);
+    
+    shelf.appendChild(bookCard);
+    
+    let findCardId = function(){
+      for (let i = 0; i < library.length; i++){
+        if ((library[i] === book)){
+    return i;
+          }
+        }   
+      }
+    
+  //activate delete button
+    cardDelete.addEventListener('click', ()=>{
+      shelf.removeChild(bookCard);
+      library.splice(findCardId(), 1);
+    })
+    
+    //activate read toggle
+    function toggleReadStatus(){
+      if(book.readStatus === true){
+        cardRead.classList.remove('completed-book');
+        cardRead.classList.add('incomplete-book');
+        cardRead.textContent = `Unread`;
+        library[findCardId()].readStatus = false;
+      } else {
+        cardRead.classList.remove('incomplete-book');
+        cardRead.classList.add('completed-book');
+        cardRead.textContent = `Read`;
+        library[findCardId()].readStatus = true;
+      }
+    }
+    
+    cardRead.addEventListener('click', toggleReadStatus);
+  
+}
+
+library.forEach((book) => {
+    renderBook(book);
+  })
