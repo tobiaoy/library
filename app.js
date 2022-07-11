117672ea3c223481eb3960c8229aabe9f8b49b72
Tobi Oyero <tobioyero@gmail.com>
2022-07-11 12:21:02 -0400

    Edited extra comment lines

--- app.js
+++ app.js
@@ -1,18 +1,4 @@
 //DOM stuff to get form values
-
-// click add book button
-// triggers form opening
-// user fills form with data
-// data is loaded from form to js
-// data is used to make a book object
-// book object is rendered into the shelf div
-// shelf div renders each object as a card
-// method should not re-render already rendered books
-// use id to know what books have already been rendered
-// book cards should have two buttons: delete and read
-// delete button should delete the card and the actual book from storage (array)
-// read button should toggle between read and unread status
-
 let library = [];
 const shelf = document.querySelector('#shelf');
 const saveButton = document.querySelector('#save-book');
@@ -178,13 +164,3 @@ function renderBook(book){
     //change rendered status
     book.rendered = true;
 }
-
-/*
-
-library.forEach((book) => {
-    if (book.rendered === false){
-    renderBook(book);
-    }
-  })
-
-  */
